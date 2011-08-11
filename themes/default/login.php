f4f85078900079fe574be8bb2e7345031c2f232b
Ghislain Phu <contact@ghislainphu.fr>
2011-08-11 19:39:57 +0200

    Add : forms

--- themes/default/login.php
+++ themes/default/login.php
@@ -3,14 +3,81 @@
 <div id="topbar"></div>
 
 <div id="main" class="container_12">
+
 	<form method="post" action="" id="login" autocomplete="off" class="prefix_4 grid_4">
-		<h1>Sign In</h1>
-		<label for id="login-name">Your Name ( <a href="#">sign up !</a> )</label>
-		<input name="login" type="text" id="login-name" autofocus/>
-		<label for id="login-password">Password ( <a href="#">forgotten ?</a> )</label>
-		<input name="password" type="password" id="login-password" />
-		<input type="submit" value="Sign In"/>
+
+		<div id="signin">
+
+			<h1>Sign In</h1>
+
+			<label for id="login-name">Your Name ( <a id="show-signup" href="#" >sign up !</a> )</label>
+			<input name="login" type="text" id="login-name" autofocus />
+
+			<label for id="login-password">Password ( <a id="show-reset" href="#">forgotten ?</a> )</label>
+			<input name="password" type="password" id="login-password" />
+
+			<input name="dologin" type="submit" value="Sign In"/>
+
+		</div>
+
+		<div id="signup">
+
+			<h1>Sign Up</h1>
+
+			<p><a href="#" class="show-signin">Show sign in</a></p>
+
+			<label for id="signup-name">Your Name</label>
+			<input name="login" type="text" id="signup-name" />
+
+			<label for id="signup-email">Email</label>
+			<input name="email" type="text" id="signup-email" />
+
+			<label for id="signup-password-1">Password</label>
+			<input name="password-1" type="password" id="signup-password-1" />
+
+			<label for id="signup-password-2">Password ( Again )</label>
+			<input name="password-2" type="password" id="signup-password-2" />
+			
+			<input name="dosignup" type="submit" value="Sign Up"/>
+
+		</div>
+
+		<div id="reset">
+
+			<h1>Reset Password</h1>
+
+			<p><a href="#" class="show-signin">Show sign in</a></p>
+
+			<label for id="reset-name">Username</label>
+			<input name="login" type="text" id="reset-name" />
+
+			<label for id="reset-email">Email</label>
+			<input name="email" type="text" id="reset-email" />
+
+			<input name="doreset" type="submit" value="Reset Password"/>
+
+		</div>
+
 	</form>
+
 </div>
+
+<script type="text/javascript">
+	$('#signup').hide();
+	$('#reset').hide();
+	$('#show-signup').click(function(){
+		$('#signin').hide();
+		$('#signup').show(210);
+	});
+	$('#show-reset').click(function(){
+		$('#signin').hide();
+		$('#reset').show(210);
+	});
+	$('.show-signin').click(function(){
+		$('#signup').hide();
+		$('#reset').hide();
+		$('#signin').show(210);
+	});
+</script>
 
 <?php include('footer.php'); ?>
