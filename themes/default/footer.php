6665fd11f7534a8398872c001aef873796560f41
Ghislain Phu <contact@ghislainphu.fr>
2011-08-11 17:58:45 +0200

    Separate files

---
+++ themes/default/footer.php
@@ -0,0 +1,5 @@
+		<script type="text/javascript">
+			$("select, input:checkbox, input:radio, input:submit, input:button").uniform();
+		</script>
+	</body>
+</html>
---
+++ themes/default/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Stanislas UI Design</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="style/style.css" />
+		<script src="js/jquery-1.6.2.min.js"></script>
+		<script src="js/jquery.uniform.min.js"></script>
+	</head>
+	<body>
