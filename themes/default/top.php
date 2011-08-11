f37967d613fac9e36b2d91e43a45dc4f04b97e35
Ghislain Phu <contact@ghislainphu.fr>
2011-08-11 23:22:13 +0200

    Outsourced

---
+++ themes/default/top.php
@@ -0,0 +1,11 @@
+<div id="topbar">
+	<ul>
+		<li>Ghislain Phu</li>
+		<li><a href="#">New Snippet</a></li>
+		<li><a href="#">Browse</a></li>
+		<li><a href="#">Search</a></li>
+		<li><a href="#">Settings</a></li>
+		<li><a href="#">Admin</a></li>
+		<li><a href="#">Logout</a></li>
+	</ul>
+</div>
