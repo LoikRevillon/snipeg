e8b73a9a1899751dad6bdc20dbe933d754a2f2da
Ghislain Phu <contact@ghislainphu.fr>
2011-08-12 14:28:18 +0200

    Add : Instant Search & php's htmlspecialchars() equivalent

--- themes/default/js/scripts.js
+++ themes/default/js/scripts.js
@@ -45,8 +45,9 @@ $(document).ready(function() {
 			request.abort();
 
 		runningRequest = true;
-		request = $.getJSON(requestPage, { q:$q.val() }, function(data) {
-			showResults(data, $q.val());
+		request = $.getJSON(requestPage, { 'query': $q.val() }, function(data) {
+			if(data != null)
+				showResults(data, $q.val());
 			runningRequest = false;
 		});
 
@@ -55,15 +56,35 @@ $(document).ready(function() {
 			var result = '';
 
 			$.each(data, function(i, item){
-				result += '<div class="result">';
-				result += '<h2><a href="#">' + data.title + '</a></h2>';
-				result += '<p>' + data.post +'</p>';
+
+				// PHP timestamp : seconds, Javascript timestamp : milliseconds
+				var update = new Date();
+				update.setTime(item.lastUpdate * 1000);
+
+				result += '<div class="result-line">';
+				result += '<div class="grid_7">';
+				result += '<h4><a href="#">' + protect(item.name) + '</a></h4>';
+				result += '<p>' + protect(item.comment) + '</p>';
+				result += '<p>Published on ' + update.toLocaleString() + ' in category ' + protect(item.category)  + '</p>';
 				result += '</div>';
+				result += '<div class="prefix_1 grid_4">';
+				result += '<div class="tags">';
+
+				$.each(item.tags.split(','), function(j, itm) {
+					result += '<a href="#">' + protect(itm) + '</a>';
+				});
+
+				result += '</div>';
+				result += '</div>';
+				result += '</div>';
+
 			});
 
 			$('div#results').html(result);
 
 		}
+
+		$('#search-head').html('<h1>Results for : ' + protect($('#query').val()) + '</h1>');
 
 		$('form').submit(function(e){
 			e.preventDefault();
@@ -72,6 +93,16 @@ $(document).ready(function() {
 	});
 
 });
+
+/*
+ * htmlspecialchars equivalent
+*/
+
+function protect(string) {
+
+	return $('<span>').text(string).html();
+
+}
 
 /*
  * Uniform
