ee8f6494a0890cd115efaada708fdd0da6f1e61b
Ghislain Phu <contact@ghislainphu.fr>
2011-08-12 16:14:39 +0200

    Add : browse page template

---
+++ themes/default/browse.php
@@ -0,0 +1,151 @@
+<?php include('header.php'); ?>
+
+<?php include('top.php'); ?>
+
+<div id="main" class="container_12">
+
+	<div id="browse">
+
+		<h1>Browse (Page 1 of 6)</h1>
+
+		<div class="browse-line">
+
+			<div class="grid_7">
+
+				<h4><a href="#">Enable Hidden Admin Feature displaying ALL Site Settings</a></h4>
+				<p>This little piece of code does something pretty cool. It will add an additional option to your settings menu with a link to "all settings".</p>
+				<p>Published on Fri Aug 12 2011 11:15:53 GMT+0200 (CEST) in category Wordpress</p>
+
+			</div>
+
+			<div class="prefix_1 grid_4">
+
+				<div class="tags">
+
+					<a href="#">wordpress</a>
+					<a href="#">hidden</a>
+					<a href="#">admin</a>
+					<a href="#">settings</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="browse-line">
+
+			<div class="grid_7">
+
+				<h4><a href="#">Enable Hidden Admin Feature displaying ALL Site Settings</a></h4>
+				<p>This little piece of code does something pretty cool. It will add an additional option to your settings menu with a link to "all settings".</p>
+				<p>Published on Fri Aug 12 2011 11:15:53 GMT+0200 (CEST) in category Wordpress</p>
+
+			</div>
+
+			<div class="prefix_1 grid_4">
+
+				<div class="tags">
+
+					<a href="#">wordpress</a>
+					<a href="#">hidden</a>
+					<a href="#">admin</a>
+					<a href="#">settings</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="browse-line">
+
+			<div class="grid_7">
+
+				<h4><a href="#">Enable Hidden Admin Feature displaying ALL Site Settings</a></h4>
+				<p>This little piece of code does something pretty cool. It will add an additional option to your settings menu with a link to "all settings".</p>
+				<p>Published on Fri Aug 12 2011 11:15:53 GMT+0200 (CEST) in category Wordpress</p>
+
+			</div>
+
+			<div class="prefix_1 grid_4">
+
+				<div class="tags">
+
+					<a href="#">wordpress</a>
+					<a href="#">hidden</a>
+					<a href="#">admin</a>
+					<a href="#">settings</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="browse-line">
+
+			<div class="grid_7">
+
+				<h4><a href="#">Enable Hidden Admin Feature displaying ALL Site Settings</a></h4>
+				<p>This little piece of code does something pretty cool. It will add an additional option to your settings menu with a link to "all settings".</p>
+				<p>Published on Fri Aug 12 2011 11:15:53 GMT+0200 (CEST) in category Wordpress</p>
+
+			</div>
+
+			<div class="prefix_1 grid_4">
+
+				<div class="tags">
+
+					<a href="#">wordpress</a>
+					<a href="#">hidden</a>
+					<a href="#">admin</a>
+					<a href="#">settings</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="browse-line">
+
+			<div class="grid_7">
+
+				<h4><a href="#">Enable Hidden Admin Feature displaying ALL Site Settings</a></h4>
+				<p>This little piece of code does something pretty cool. It will add an additional option to your settings menu with a link to "all settings".</p>
+				<p>Published on Fri Aug 12 2011 11:15:53 GMT+0200 (CEST) in category Wordpress</p>
+
+			</div>
+
+			<div class="prefix_1 grid_4">
+
+				<div class="tags">
+
+					<a href="#">wordpress</a>
+					<a href="#">hidden</a>
+					<a href="#">admin</a>
+					<a href="#">settings</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<div id="paging">
+
+		<a href="#">First</a>
+		<a href="#">1</a>
+		<a href="#">2</a>
+		<a href="#">3</a>
+		<a href="#">4</a>
+		<a href="#">Last</a>
+
+	</div>
+
+</div>
+
+<?php include('footer.php'); ?>
