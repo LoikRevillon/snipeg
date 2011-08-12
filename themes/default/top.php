5e0eacbb4faf51c2d9b54d93f6ed7625248ba2ad
Ghislain Phu <contact@ghislainphu.fr>
2011-08-12 16:13:43 +0200

    Add : Links for debugging

--- themes/default/top.php
+++ themes/default/top.php
@@ -1,11 +1,11 @@
 <div id="topbar">
 	<ul>
 		<li>Ghislain Phu</li>
-		<li><a href="#">New Snippet</a></li>
-		<li><a href="#">Browse</a></li>
-		<li><a href="#">Search</a></li>
-		<li><a href="#">Settings</a></li>
-		<li><a href="#">Admin</a></li>
-		<li><a href="#">Logout</a></li>
+		<li><a href="new.php">New Snippet</a></li>
+		<li><a href="browse.php">Browse</a></li>
+		<li><a href="search.php">Search</a></li>
+		<li><a href="settings.php">Settings</a></li>
+		<li><a href="admin.php">Admin</a></li>
+		<li><a href="?logout">Logout</a></li>
 	</ul>
 </div>
