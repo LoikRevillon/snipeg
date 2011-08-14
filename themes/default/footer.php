17cda9c022f426484039ab36f319764f7240ec21
Ghislain Phu <contact@ghislainphu.fr>
2011-08-14 23:15:55 +0200

    Bugfix

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -3,8 +3,8 @@
 				if($.browser.mozilla || $.browser.opera) {
 					var lnk = $('<link>');
 					lnk.attr({
-						rel: 'stylesheet'
-						href: '<?php echo $Theme->location; ?>style/style-fix.css',
+						rel: 'stylesheet',
+						href: '<?php echo $Theme->location; ?>style/style-fix.css'
 					});
 					$('head').append(lnk);
 				}
