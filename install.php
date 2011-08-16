34ca7bb523cabbbb6a4893ba018f3dee12dd0d04
Loïk Révillon <revillon.loik@hotmail.fr>
2011-08-16 23:34:19 +0200

    BUG FIX : misspelling in SQL request, SQL quote at wrong place.

--- install.php
+++ install.php
@@ -46,7 +46,7 @@ if(isset($_POST['init'])) {
 
 	$db = PDOSQLite::getDBLink(false);
 
-	$db->query('CREATE TABLE IF NOT EXISTS `snippets` (`name` VARCHAR(255), `id_user` INT(1), `last_update` BIGINT(32), `content TEXT`, `language` INT(1), `comment` TEXT, `category` VARCHAR(80), `tags` TEXT, `private` INT(1))');
+	$db->query('CREATE TABLE IF NOT EXISTS `snippets` (`name` VARCHAR(255), `id_user` INT(1), `last_update` BIGINT(32), `content` TEXT, `language` INT(1), `comment` TEXT, `category` VARCHAR(80), `tags` TEXT, `private` INT(1))');
 
 	$db->query('CREATE TABLE IF NOT EXISTS `users` (`admin` INT(1), `name` VARCHAR(30), `email` VARCHAR(80), `avatar` INT(1), `password` VARCHAR(64), `locked` INT(1), `theme` VARCHAR(50), `language` VARCHAR(10), `favorite_lang` TEXT)');
 
