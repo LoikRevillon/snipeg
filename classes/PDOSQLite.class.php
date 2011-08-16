d31d3e93440f6d93e6a88ad0ecc268a97e154bb9
Ghislain Phu <contact@ghislainphu.fr>
2011-08-16 14:27:32 +0200

    Patch for install.php, Add optionnal parameter

--- classes/PDOSQLite.class.php
+++ classes/PDOSQLite.class.php
@@ -6,12 +6,12 @@ class PDOSQLite {
 
 	private static $_instance;
 
-	private function __construct() {
+	private function __construct($checkExists = true) {
 
 		global $Lang;
 
 		try {
-			if(!file_exists(DB_NAME) AND !is_dir(DB_NAME))
+			if($checkExists AND !file_exists(DB_NAME) AND !is_dir(DB_NAME))
 				throw new Exception($Lang->warning_no_database_initialized);
 			self::$_dbLink = new PDO('sqlite:' . DB_NAME);
 			self::$_dbLink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -22,10 +22,10 @@ class PDOSQLite {
 
 	}
 
-	public static function getDBLink() {
+	public static function getDBLink($checkExists = true) {
 
 		if(!isset(self::$_instance) OR !isset($_dbLink))
-			self::$_instance = new self();
+			self::$_instance = new self($checkExists);
 
 		return self::$_dbLink;
 
